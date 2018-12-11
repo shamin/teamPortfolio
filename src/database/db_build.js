67e78e21d98bc4ed3a53bc2c937f8d90d5a2f901
Michal Weizman <m.weizman@gmail.com>
2018-12-11 20:47:13 +0000

    Removed comments from db_build.js

--- src/database/db_build.js
+++ src/database/db_build.js
@@ -2,24 +2,8 @@ const fs = require("fs");
 const dbConnection = require("./db_connection");
 const sql = fs.readFileSync(`${__dirname}/db_build.sql`).toString();
 
-// const dbBuilder = () => {
-//   const sql = fs.readFileSync(`${__dirname}/db_build.sql`).toString();
-//   dbConnection.query(sql, (err, result) => {
-//     if (err) {
-//       console.log(err, "error");
-//     } else {
-//       console.log("database created");
-//       dbConnection.end(() => {
-//         console.log("connection closed");
-//       });
-//     }
-//   });
-// };
-
 const dbBuilder = cb => dbConnection.query(sql, cb);
 
+// dbBuilder();
+
 module.exports = dbBuilder;
-
-//dbBuilder();
-
-// module.exports = dbBuilder;
